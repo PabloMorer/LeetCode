6ff9c1a23b3e4ade983e61d44c8ec2a53a20fe4b
pablomorer <pablomorer28@gmail.com>
2023-11-23 11:41:27 +0100

    main removed

--- Algorithms/25. Reverse Nodes in k-Group/Project1/25_Reverse_Nodes_in_k_Group.cpp
+++ Algorithms/25. Reverse Nodes in k-Group/Project1/25_Reverse_Nodes_in_k_Group.cpp
@@ -88,26 +88,3 @@ public:
         return ret;
     }
 };
-
-int main() {
-
-    Solution s;
-
-    ListNode* n1 = new ListNode(1);
-    ListNode* n2 = new ListNode(2);
-    ListNode* n3 = new ListNode(3);
-    ListNode* n4 = new ListNode(4);
-    ListNode* n5 = new ListNode(5);
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-
-    ListNode* r = s.reverseKGroup(n1, 1);
-
-    while (r != NULL) {
-        std::cout << r->val << " ";
-        r = r->next;
-    }
-
-}
