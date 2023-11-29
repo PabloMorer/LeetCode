d3e3288f210d34e3ecd09e0f3c2e032dcbdb4190
PabloMorer <61006664+PabloMorer@users.noreply.github.com>
2023-11-29 16:51:13 +0100

    Create 4_median_of_two_sorted_arrays.cpp

---
+++ Algorithms/4. Median of Two Sorted Arrays/4_median_of_two_sorted_arrays.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        for (int i = 0; i < nums2.size(); i++) {
+            nums1.push_back(nums2[i]);
+        }
+
+        sort(nums1.begin(), nums1.end());
+        return 0.5 * (nums1[(nums1.size() - 1) / 2] + nums1[nums1.size() / 2]);
+    }
+};
