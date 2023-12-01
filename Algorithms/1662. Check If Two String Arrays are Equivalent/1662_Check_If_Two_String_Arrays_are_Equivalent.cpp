f47ebaacab2eb7ea2fe4c9124d4b1750f346258c
PabloMorer <61006664+PabloMorer@users.noreply.github.com>
2023-12-01 12:09:41 +0100

    Create 1662_Check_If_Two_String_Arrays_are_Equivalent.cpp

---
+++ Algorithms/1662. Check If Two String Arrays are Equivalent/1662_Check_If_Two_String_Arrays_are_Equivalent.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = "";
+        string s2 = "";
+        for (auto& a : word1) {
+            s1 += a;
+        }
+        for (auto& b : word2) {
+            s2 += b;
+        }
+
+        return s1 == s2;
+    }
+};
