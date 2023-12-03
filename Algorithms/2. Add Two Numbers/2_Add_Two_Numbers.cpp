4c62d28447eb7d83bd925bf7052b400404d6e65b
PabloMorer <61006664+PabloMorer@users.noreply.github.com>
2023-12-03 21:31:26 +0100

    Create 2_Add_Two_Numbers.cpp

---
+++ Algorithms/2. Add Two Numbers/2_Add_Two_Numbers.cpp
@@ -0,0 +1,46 @@
+ 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+      
+        ListNode* r = new ListNode();
+        ListNode* r_ant = r;
+        ListNode* ini = r;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL) {
+         
+            int n1 = 0;
+            int n2 = 0;
+            if (l1 != NULL) {
+                n1 = l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                n2 = l2->val;
+                l2 = l2->next;
+            }
+            int sum = n1 + n2 + carry;
+
+            if (sum >= 10) {
+                r->val = sum % 10;
+                carry = sum / 10;
+            }
+            else {
+                r->val = sum;           
+                carry = 0;
+            }
+
+            
+            if (l1 != NULL || l2 != NULL) {
+                r->next = new ListNode();
+                r = r->next;
+            }
+
+        }
+        if (carry > 0) {
+            r->next = new ListNode(carry);
+        }
+
+        return ini;
+    }
+};
