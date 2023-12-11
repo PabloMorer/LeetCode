2a72e239cb27eee8c083c5e08c708c50b8fa7465
PabloMorer <61006664+PabloMorer@users.noreply.github.com>
2023-12-11 10:43:54 +0100

    Create 5_Longest_Palindromic_Substring.cpp

---
+++ Algorithms/5. Longest Palindromic Substring/5_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        int max = 1;
+        int a = 0;
+        int b = 0;
+        vector<vector<bool>> dp(s.length(), vector<bool>(s.length(), false));
+
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > max) {
+                        max = i - j + 1;
+                        a = j;
+                        b = i;
+                    }
+                }
+            }
+        }
+        return s.substr(a, b - a + 1);
+    }
+};
