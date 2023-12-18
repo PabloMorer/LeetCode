66361fe41e293247cece221de7de845a886548f5
PabloMorer <61006664+PabloMorer@users.noreply.github.com>
2023-12-18 08:43:09 +0100

    Update 1913_Maximum_Product_Difference_Between_Two_Pairs.cpp

--- Algorithms/1913. Maximum Product Difference Between Two Pairs/1913_Maximum_Product_Difference_Between_Two_Pairs.cpp
+++ Algorithms/1913. Maximum Product Difference Between Two Pairs/1913_Maximum_Product_Difference_Between_Two_Pairs.cpp
@@ -9,16 +9,14 @@ public:
         int c = INT_MAX;
         int d = INT_MAX;
         
+
         for (int i = 0; i < nums.size(); i++) {
             if (a <= nums[i]) {
                 b = a;
                 a = nums[i];
-                idx_b = idx_a;
-                idx_a = i;
             }
             else if (b < nums[i]) {
                 b = nums[i];
-                idx_b = i;
             }
             if (c >= nums[i]) {
                 d = c;
