64d3fd95ef8b713d28d3562032eed01b2d8dd50d
PabloMorer <61006664+PabloMorer@users.noreply.github.com>
2023-12-18 08:41:58 +0100

    Create 1913_Maximum_Product_Difference_Between_Two_Pairs.cpp

---
+++ Algorithms/1913. Maximum Product Difference Between Two Pairs/1913_Maximum_Product_Difference_Between_Two_Pairs.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        int a = INT_MIN;
+        int b = INT_MIN;
+        int c = INT_MAX;
+        int d = INT_MAX;
+        
+        int idx_a = 0;
+        int idx_b = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (a <= nums[i]) {
+                b = a;
+                a = nums[i];
+                idx_b = idx_a;
+                idx_a = i;
+            }
+            else if (b < nums[i]) {
+                b = nums[i];
+                idx_b = i;
+            }
+            if (c >= nums[i]) {
+                d = c;
+                c = nums[i];
+            }
+            else if (d > nums[i]) {
+                d = nums[i];
+            }
+        }
+        
+        return (a * b) - (c * d);
+    }
+};
